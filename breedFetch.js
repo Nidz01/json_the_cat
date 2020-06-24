12b2f9b1c66995c114e454c6b35676c8ae3d09e8
Nida Q <65258819+Nidz01@users.noreply.github.com>
2020-06-24 16:12:03 -0400

    Update breedFetch.js
    simple code for getting requested breed description

--- breedFetch.js
+++ breedFetch.js
@@ -6,26 +6,22 @@ const request = require('request');
 const url= 'https://api.thecatapi.com/v1/breeds/search?q=';
 const breedName = process.argv.slice(2);
 
-const fetchBreedDescription = function(breedName, callback) {
+const fetchBreedDescription = function(breedName) {
   request(url + breedName, (error, description) => {
     if (error) {
       console.log("Potential error in URL");
-      return;
     } else {
-      callback(description);
+      const data = JSON.parse(description.body); // In order to scan this data like a JavaScript object, we need to convert the string version of it into an object first. 
+    if (data[0] === undefined) {
+        console.log("Search Query denied. Please search a cat breed that exists.");
+      } else {
+        console.log(data[0].description);
+      }
     }
   });
 };
 
-const printBreed = function (description) {
-  const data = JSON.parse(description.body); // In order to scan this data like a JavaScript object, we need to convert the string version of it into an object first. 
-    if (data[0] === undefined) {
-        console.log("Search Query denied. Please search a cat breed that exists.");
-      } else {
-        console.log(`DESCRIPTION: ${data[0].description}`);
-      }
-};
-
-fetchBreedDescription(breedName, printBreed);
+fetchBreedDescription(breedName);
 
 module.exports = { fetchBreedDescription };
+
