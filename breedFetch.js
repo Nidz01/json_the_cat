2648718c5f6171862a9a1021426408a317513090
Nida Q <65258819+Nidz01@users.noreply.github.com>
2020-06-24 16:13:30 -0400

    Update breedFetch.js
    when called from separate index.js file

--- breedFetch.js
+++ breedFetch.js
@@ -1,27 +1,19 @@
-/*
-Write the logic in breedFetcher.js to fetch the Siberian data from the API endpoint using request.
-*/
-
 const request = require('request');
 const url= 'https://api.thecatapi.com/v1/breeds/search?q=';
-const breedName = process.argv.slice(2);
 
-const fetchBreedDescription = function(breedName) {
+const fetchBreedDescription = function(breedName, callback) {
   request(url + breedName, (error, description) => {
     if (error) {
-      console.log("Potential error in URL");
+      callback(error);
     } else {
       const data = JSON.parse(description.body); // In order to scan this data like a JavaScript object, we need to convert the string version of it into an object first. 
     if (data[0] === undefined) {
         console.log("Search Query denied. Please search a cat breed that exists.");
       } else {
-        console.log(data[0].description);
+        callback(null, data[0].description);
       }
     }
   });
 };
 
-fetchBreedDescription(breedName);
-
 module.exports = { fetchBreedDescription };
-
